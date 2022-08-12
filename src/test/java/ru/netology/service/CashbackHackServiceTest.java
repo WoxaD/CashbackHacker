99be9c2d571e900a0c428b309309c2b8de46029e
Vadim Piskarev <woxadestroyer@gmail.com>
2022-08-12 16:10:55 +0300

    JUnit4 changes

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,24 +4,24 @@ import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
-    void shouldReturnTheMissingAmountToReceiveACashback() {
+    @org.junit.Test
+    public void shouldReturnTheMissingAmountToReceiveACashback() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int sum = 900;
 
         assertEquals(cashbackHackService.remain(sum), 100);
     }
 
-    @org.testng.annotations.Test
-    void shouldReturnTheMissingAmountToReceiveACashbackIfMoreThan1000AreSpent() {
+    @org.junit.Test
+    public void shouldReturnTheMissingAmountToReceiveACashbackIfMoreThan1000AreSpent() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int sum = 1050;
 
         assertEquals(cashbackHackService.remain(sum), 950);
     }
 
-    @org.testng.annotations.Test
-    void shouldReturnTheMissingAmountToReceiveACashbackIfNothingNeedsToBeBoughtInAddition() {
+    @org.junit.Test
+    public void shouldReturnTheMissingAmountToReceiveACashbackIfNothingNeedsToBeBoughtInAddition() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int sum = 1000;
 
